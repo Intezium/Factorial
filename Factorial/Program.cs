39a0d6643e113b8b723f8efe2602bb4ba63c1bc0
Intezium <Intezium04@gmail.com>
2018-03-17 16:56:42 +0300

    Проект завершен

--- Factorial/Program.cs
+++ Factorial/Program.cs
@@ -1,15 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Factorial
 {
     class Program
     {
+        static void Factorial(int number)
+        {
+            int factorial = 1;
+
+            for (int i = 1; i <= number; i++)
+                factorial *= i;
+
+            Console.WriteLine("\nФакториал числа {0} равен {1}", number, factorial);
+        }
+
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.Clear();
+
+                Console.WriteLine("===>[Подсчет факториала]<===");
+                Console.Write("\nВведите число: ");
+
+                try
+                {
+                    int number = int.Parse(Console.ReadLine());
+
+                    Factorial(number);
+                }
+                catch (FormatException) { Console.WriteLine("\nНекорректное число!"); }
+
+                Console.WriteLine("\nEnter - Проверить еще");
+                Console.WriteLine("Esc - Выйти");
+
+                ConsoleKey inputKey = Console.ReadKey().Key;
+
+                if (inputKey == ConsoleKey.Enter)
+                    continue;
+                else if (inputKey == ConsoleKey.Escape)
+                    break;
+            }
         }
     }
 }
